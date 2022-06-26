c449b84953ecd2e62ab8e4d842e536cba9c0ffca
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-06-26 02:47:09 -0500

    Wildflower scrape except latin from link

---
+++ wildflowerQuickstart.py
@@ -0,0 +1,21 @@
+# imports we'll use in this example
+from spatula import (
+    HtmlPage, HtmlListPage, CSS, XPath, SelectorError
+)
+
+
+class WildflowerList(HtmlListPage):
+    source = "https://www.minnesotawildflowers.info/page/plants-by-name"
+
+    # each row represents an employee
+    selector = CSS(".wfList tbody tr")
+
+    def process_item(self, item):
+        # this function is called for each <tr> we get from the selector
+        # we know there are 4 <tds>
+        Latin, common = item.getchildren()
+        return dict(
+            Latin=Latin.text,  # next thing to research is scraping just the <a> text from these links
+            common=common.text,
+        )
+
