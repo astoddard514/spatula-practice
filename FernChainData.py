d3f385acbf7573ac682ef447553cdfec2b7e5b5b
astoddard514 <angela.haugen.stoddard@gmail.com>
2022-06-27 01:30:09 -0500

    Fiddle with Fern page selectors, update ReadMe

---
+++ FernChainData.py
@@ -0,0 +1,23 @@
+from spatula import (
+    HtmlPage, HtmlListPage, CSS, XPath, SelectorError
+)
+
+class FernList(HtmlListPage):
+     # by providing this here, it can be omitted on the command line
+     # useful in cases where the scraper is only meant for one page
+     source = "https://www.minnesotawildflowers.info/page/ferns-and-fern-allies"
+
+     # each row represents an employee
+     selector = CSS(".thumbs ul li")
+
+     def process_item(self, item):
+         # this function is called for each <tr> we get from the selector
+         # we know there are 4 <tds>
+        common = item.getchildren()
+         # return EmployeeDetail(
+        return dict(
+            common=common.a.get_text(),
+            )
+             # source=XPath("./a/@href").match_one(details),
+        
+
--- README.md
+++ README.md
@@ -2,3 +2,11 @@
 Working through "A First Scraper" by James Turk
 
 https://jamesturk.github.io/spatula/scraper-basics/
+
+I go through the tutorial as presented, and then adapt it to scrape native plant info from https://www.minnesotawildflowers.info/. 
+
+Things I worked through: 
+
+    - Reviewd how to set up my python virtual environment, and worked through import and dependency errors.
+    - Researched how to scrape text from an anchor tag/link.
+    - Researched how to scrape to a CSV file.
